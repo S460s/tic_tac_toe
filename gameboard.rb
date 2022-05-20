4362da5538c68b2c0e595048b9370727120361dc
S460 <rommel.e@protonmail.ch>
2022-05-20 13:11:05 +0300

    game loop works, no win check

--- gameboard.rb
+++ gameboard.rb
@@ -4,22 +4,44 @@ class Gameboard
   attr_reader :board
 
   def initialize(size)
+    @turns_played = 0
     @size = size
-    @board = Array.new(size) { |i| Array.new(size) { |j| (j + i * size).to_s.center(size) } }
+    @board = Array.new(size) { |i| Array.new(size) { |j| (j + i * size).to_s } }
     @players = []
   end
 
-  def valid_index?(index)
-    return false if index > @size * @size
+  def start_game
+    if @players.length != 2
+      puts 'Wrong number of players, shutting down.'
+      return false
+    end
 
-    !ALLOWED_SIGNS.include?(@board.flatten[index].strip)
+    until game_over?
+      @players.each do |player|
+        update_board(player)
+        break if game_over?
+      end
+    end
+
+    puts 'GG'
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  private
+
+  def game_over?
+    @turns_played == @size * @size
   end
 
   def update_board(player)
     index = player.play
 
     if valid_index?(index)
-      @board[index / @size][index % @size] = player.sign.center(@size)
+      @board[index / @size][index % @size] = player.sign
+      @turns_played += 1
       print_board
     else
       puts "#{index} is an invalid option."
@@ -27,14 +49,16 @@ class Gameboard
     end
   end
 
-  def add_player(player)
-    @players << player
-  end
-
   def print_board
     @board.each do |row|
-      row.each { |sign| print "|#{sign}|" }
+      row.each { |sign| print "|#{sign.center(@size)}|" }
       puts
     end
+  end
+
+  def valid_index?(index)
+    return false if index > @size * @size
+
+    !ALLOWED_SIGNS.include?(@board.flatten[index])
   end
 end
