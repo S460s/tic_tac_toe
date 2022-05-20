860fa599f72ca4c1997f950fa31df9735533d2b2
S460 <rommel.e@protonmail.ch>
2022-05-20 12:25:29 +0300

    add a play method

--- player.rb
+++ player.rb
@@ -5,4 +5,13 @@ class Player
     @sign = sign
     @name = name
   end
+
+  def play(isFirst = true)
+    puts "#{name}'s turn" if isFirst
+    ans = gets.chomp
+    return ans.to_i if ans.match?(/\A-?\d+\Z/)
+
+    puts 'Invalid input, please try again.'
+    play false
+  end
 end
